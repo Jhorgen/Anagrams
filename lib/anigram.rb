9c2991ea8e5701f178aa542eb5415936363490e9
Josh <Joshua.horgen@gmail.com>
2019-07-19 09:21:09 -0700

    Set up class, method, etc

--- lib/anigram.rb
+++ lib/anigram.rb
@@ -1,2 +1,23 @@
 require ('pry')
 require ('colorize')
+
+class AnigramMachine
+attr_accessor(:word)
+def initialize
+@word = word
+end
+
+def anigram(word)
+@word = "abcdefg"
+if @word.include? "cde"
+puts "String includes 'cde'"
+end
+
+
+
+
+end
+
+
+word = gets.chomp
+anigram_machine = AnigramMachine.new()
