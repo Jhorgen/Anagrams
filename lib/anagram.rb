b992784c03b058a317e15b535d216b490271565b
Josh <Joshua.horgen@gmail.com>
2019-07-20 19:06:01 -0700

    Pass more tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,35 +1,25 @@
 require ('pry')
 
 class AnagramMachine
-attr_accessor(:word, :word1)
+  attr_accessor(:word, :word1)
 
-def initialize(word, word1)
-@word = word.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
-@word1 = word1.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
-end
-
-def anagram(word, word1)
-  if  @word.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) } || @word1.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) }
-    p "This is a word"
-    p @word
-    p @word1
-  else
-    return p "Not a word"
+  def initialize(word, word1)
+    @word = word.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
+    @word1 = word1.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
   end
-if @word != @word1
-  p "You have submitted an antigram, nothing matches!"
-elsif @word == @word1
-  p "This is an anagram"
+
+  def anagram(word, word1)
+    if  @word.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) } || @word1.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) }
+      p "This is a word"
+      p @word
+      p @word1
+    else
+      return p "Not a word"
+    end
+    if @word != @word1
+      p "You have submitted an antigram, nothing matches!"
+    elsif @word == @word1
+      p "This is an anagram"
+    end
+  end
 end
-end
-end
-
-
-
-
-# puts 'enter first word'
-# word = gets.chomp
-# puts "enter second word"
-# word1 = gets.chomp
-# anagram_machine = AnagramMachine.new()
-# anagram_machine.anagram(word, word1)
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -7,10 +7,27 @@ describe('anagram') do
   #   expect(test_this.anagram('ruby', 'ruby')).to(eq("These arrays are the same!"))
   # end
 
-#First passing test is no longer relevent to code.
+  #First passing test is no longer relevent to code.
 
   it("should check if two words are anagrams") do
     test_this = AnagramMachine.new('eat', 'tea')
     expect(test_this.anagram('eat', 'tea')).to(eq("This is an anagram"))
-end
+  end
+  it("should check for vowels") do
+    test_this = AnagramMachine.new('eat', 'tea')
+    expect(test_this.anagram('eat', 'tea')).to(eq("This is a word"))
+  end
+  it("should end the script when an invalid word is entered") do
+    test_this = AnagramMachine.new('sfs', 'sfs')
+    expect(test_this.anagram('sfs', 'sfs')).to(eq("Not a word"))
+  end
+  # it("should check if two words are anagrams") do
+  #   test_this = AnagramMachine.new('eat', 'tea')
+  #   expect(test_this.anagram('eat', 'tea')).to(eq("This is an anagram"))
+  # end
+  # it("should check if two words are anagrams") do
+  #   test_this = AnagramMachine.new('eat', 'tea')
+  #   expect(test_this.anagram('eat', 'tea')).to(eq("This is an anagram"))
+  # end
+
 end
