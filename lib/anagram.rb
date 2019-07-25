9036d38aa3b8689dd13733e3a9f744f9f152105f
Josh <Joshua.horgen@gmail.com>
2019-07-25 15:22:20 -0700

    Refactor, add README.md file, add passing tests

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,18 +4,20 @@ class AnagramMachine
   attr_accessor(:word, :word1)
 
   def initialize(word, word1)
-    @@word = word.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
-    @@word1 = word1.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
+    @word = word.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
+    @word1 = word1.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
   end
 
-  def anagram(word, word1)
-    if @@word == @@word1
+  def anagram
+    if @word == @word1
       p "This is an anagram"
+    else
+      p "They don't seem to match.."
     end
   end
 
   def vow_check
-    if  @@word.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) } || @@word1.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) }
+    if  @word.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) } || @word1.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) }
       p "This is a word"
     else
       return p "Not a word"
@@ -23,9 +25,23 @@ class AnagramMachine
   end
 
   def antigram_check
-    if @@word != @@word1
-      p "You have submitted an antigram, nothing matches!"
-    else
+    is_antigram = true
+    for letters in @word do
+      if @word1.include?(letters)
+        is_antigram = false
+        break
+      end
     end
+    if is_antigram
+      return "You have submitted an antigram, nothing matches!"
+    end
+    return "This neither an antigram or anagram"
   end
 end
+
+
+
+# if @word != @word1
+#   p "You have submitted an antigram, nothing matches!"
+# else
+# end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -11,23 +11,30 @@ describe('anagram') do
 
   it("should check if two words are anagrams") do
     test_this = AnagramMachine.new('eat', 'tea')
-    expect(test_this.anagram('eat', 'tea')).to(eq("This is an anagram"))
+    expect(test_this.anagram()).to(eq("This is an anagram"))
   end
+
+  it("should check if two words are not anagrams") do
+    test_this = AnagramMachine.new('not', 'same')
+    expect(test_this.anagram()).to(eq("They don't seem to match.."))
+  end
+
   it("should check for vowels") do
     test_this = AnagramMachine.new('tea', 'eat')
     expect(test_this.vow_check()).to(eq("This is a word"))
   end
-  it("should end the script when an invalid word is entered") do
+  it("should check for invalid words") do
     test_this = AnagramMachine.new('sdf', 'sdf')
     expect(test_this.vow_check()).to(eq("Not a word"))
   end
-  # it("should check if two words are anagrams") do
-  #   test_this = AnagramMachine.new('eat', 'tea')
-  #   expect(test_this.anagram('eat', 'tea')).to(eq("This is an anagram"))
-  # end
-  # it("should check if two words are anagrams") do
-  #   test_this = AnagramMachine.new('eat', 'tea')
-  #   expect(test_this.anagram('eat', 'tea')).to(eq("This is an anagram"))
-  # end
+  it("should check for antigrams") do
+    test_this = AnagramMachine.new('test', 'fail')
+    expect(test_this.antigram_check()).to(eq("You have submitted an antigram, nothing matches!"))
+  end
+
+  it("should check for neither antigrams or anagrams ") do
+    test_this = AnagramMachine.new('eat', 'tea')
+    expect(test_this.antigram_check()).to(eq("This neither an antigram or anagram"))
+  end
 
 end
