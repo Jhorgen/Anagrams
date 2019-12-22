7562475f9c1a26a7c2284af6b1a35f3dbb74edf7
Josh <Joshua.horgen@gmail.com>
2019-12-22 15:48:26 -0800

    Fix argument error

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class AnagramMachine
     @word1 = word1.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
   end
 
-  def anagram
+  def anagram(word, word1)
     if @word == @word1
       p "This is an anagram"
     else
@@ -18,9 +18,9 @@ class AnagramMachine
 
   def vow_check
     if  @word.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) } || @word1.any?{ |x| ["a", "e", "i", "o", "u"].include?(x) }
-      p "This is a word"
+      p "This is a word. (passed word check)"
     else
-      return p "Not a word"
+      return p "Not a word, please try again. (failed word check)"
     end
   end
 
